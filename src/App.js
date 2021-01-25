e421fa5c2f68813fd611fcf94ae9d3bdd51d771f
robinfang <thefmajia@gmail.com>
2021-01-26 00:25:54 +0800

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import {Button} from '@material-ui/core/';
+import { Button } from '@material-ui/core/';
 import './App.css';
 
-function App() {
-   const [state, setState] = React.useState({
-     time_list : []
-   });
-   const tap = () => {
-     setState({...state, time_list:[...state.time_list, Date.now()]})
-   }
-  return (
-    <div className="App">
-      <Button size="large" variant="contained" color="primary" onClick={tap}>
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { time_list: [] };
+    }
+
+    tap() {
+        this.setState(state => ({
+            time_list: [...state.time_list, Date.now()]
+        }));
+    }
+    render() {
+        return (
+            <div className="App">
+      <Button size="large" variant="contained" color="primary" onClick={this.tap}>
         Tap
       </Button>
       <p>
       {this.state.time_list}
       </p>
     </div>
-  );
+        );
+    }
 }
 
 export default App;
-
